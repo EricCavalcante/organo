15913d7923c57b041d762b9bdc76a6b24e1ebe62
Eric <ericcavalcante.dev@gmail.com>
2023-11-16 02:44:29 -0300

    enviando o projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Banner from './componentes/Banner/Banner';
+import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
+  
+  const times = [
+    {
+      nome: 'Duelistas',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
+  },
+  {
+      nome: 'Sentinelas',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
+  },
+  {
+      nome: 'Iniciadores',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8',
+  },
+  {
+      nome: 'Controladores',
+      corPrimaria: '#D86EBF',
+      corSecundaria: '#FAE5F5',
+  },
+  {
+      nome: 'Flex',
+      corPrimaria: '#FEBA05',
+      corSecundaria: '#FFF5D9',
+  },
+  
+  ]
+  
+  const [colaboradores, setColaboradores] = useState([])
+  
+  const aoNovoColaboradorAdicionado = (colaborador) =>{
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner/>
+      
+      <Formulario 
+        aoColaboradorCadastrado={aoNovoColaboradorAdicionado}
+        times={times.map(time => time.nome)}/>
+      
+      {times.map(time => <Time 
+                              key={time.nome} 
+                              nome={time.nome} 
+                              corPrimaria={time.corPrimaria} 
+                              corSecundaria={time.corSecundaria}
+                              colaboradores={colaboradores.filter(colaborador => colaborador.time == time.nome )} />)}
     </div>
   );
 }
